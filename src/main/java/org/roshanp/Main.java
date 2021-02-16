ad8467e7e25c200486ab27b4371a0e2fc1a3613e
Roshan Prabhakar <roshan@aprabhakar.com>
2021-02-16 08:41:48 -0800

    initial commit

---
+++ src/main/java/org/roshanp/Main.java
@@ -0,0 +1,9 @@
+package org.roshanp;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/org/roshanp/cubeoperations/Cube.java
@@ -0,0 +1,38 @@
+package org.roshanp.cubeoperations;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public class Cube {
+
+    private Point[][][] CUBE;
+
+    public Cube(BufferedImage image) {
+        this();
+        init(image);
+    }
+
+    public Cube() {
+        CUBE = new Point[255][255][255];
+    }
+
+    public void init(BufferedImage image) {
+
+        for (int r = 0; r < image.getHeight(); r++) {
+            for (int c = 0; c < image.getWidth(); c++) {
+
+                Color pixel = new Color(image.getRGB(c, r));
+
+                if (CUBE[pixel.getRed()][pixel.getGreen()][pixel.getBlue()] == null)
+                    CUBE[pixel.getRed()][pixel.getGreen()][pixel.getBlue()] = new Point(c, r);
+
+            }
+        }
+    }
+
+    public Point get(int r, int g, int b) {
+        return CUBE[r][g][b];
+    }
+
+}
